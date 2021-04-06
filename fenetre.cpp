1ed3ce6bc7000a3133973a6c206c3db315476b10
AxelClm <46006583+AxelClm@users.noreply.github.com>
2021-04-06 19:30:38 +0200

    Resize

--- fenetre.cpp
+++ fenetre.cpp
@@ -1,16 +1,53 @@
 #include "fenetre.h"
 
 Fenetre::Fenetre(){
+    m_VBGaucheLayout = new QVBoxLayout();
+    m_HBMidLayout = new QHBoxLayout();
+    m_VBDroiteLayout = new QVBoxLayout();
+    m_HBLayout = new QHBoxLayout();
+    m_HBLayout->addLayout(m_VBGaucheLayout);
+    m_HBLayout->addLayout(m_HBMidLayout);
+    m_HBLayout->addLayout(m_VBDroiteLayout);
+    this->setLayout(m_HBLayout);
+    //Creation menu gauche
+    m_VBGaucheLayout->setAlignment(Qt::AlignTop);
+    m_ajoutSerie = new QPushButton(this);
+    m_ajoutSerie->setText("Ajout Serie");
+    m_VBGaucheLayout->addWidget(m_ajoutSerie);
+    m_fusionSerie = new QPushButton(this);
+    m_fusionSerie->setText("Fusion Serie");
+    m_VBGaucheLayout->addWidget(m_fusionSerie);
+    m_VBGaucheLayout->setContentsMargins(0,10,0,10);
+    //Creation menu millieu (tmp)
     m_imgD = new ImageDisplayer(this);
-    this->setFixedSize(512,512);
-    dicomImage a = m_dcm.parseDicom("C:/1-089.dcm");
+    m_HBMidLayout->addWidget(m_imgD);
+    dicomImage a = m_dcm.parseDicom("C:/1-110.dcm");
     a.generateImage();
     m_imgD->linkImage(a.getImage());
-    update();
+    //Creation menu gauche
+    this->setGeometry(0,0,900,600);
+    QSize NSize = QSize(850,600);
+    QSize OSize = QSize(this->size());
+    QResizeEvent * ev = new  QResizeEvent(NSize,OSize);
+    QCoreApplication::postEvent(this,ev);
 }
 
 Fenetre::~Fenetre()
 {
+}
+void Fenetre::resizeEvent(QResizeEvent* event){
+    int mGRatio = 20;
+    int mGWidth = event->size().width()*mGRatio/100;
+    if (mGWidth > 150){
+        mGWidth = 150;
+    }
+    qDebug() << mGWidth;
+    m_ajoutSerie->resize(mGWidth,m_ajoutSerie->height());
+    m_fusionSerie->resize(mGWidth,m_fusionSerie->height());
+    m_ajoutSerie->setContentsMargins(0,10,0,10);
+    m_fusionSerie->setContentsMargins(0,10,0,10);
+    QWidget::resizeEvent(event);
+
 }
 
 
--- fenetre.h
+++ fenetre.h
@@ -10,12 +10,24 @@
 #include <imagedisplayer.h>
 #include <dicomreader.h>
 #include <dicomimage.h>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QResizeEvent>
+#include <QCoreApplication>
 class Fenetre : public QWidget
 {
     Q_OBJECT
 private:
     DicomReader m_dcm;
     ImageDisplayer* m_imgD;
+    QHBoxLayout* m_HBLayout;
+    QHBoxLayout* m_HBMidLayout;
+    QVBoxLayout* m_VBDroiteLayout;
+    QVBoxLayout* m_VBGaucheLayout;
+    QPushButton* m_ajoutSerie;
+    QPushButton* m_fusionSerie;
+    void resizeEvent(QResizeEvent* event);
 public:
     Fenetre();
     virtual ~Fenetre();
